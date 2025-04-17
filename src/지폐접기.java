3ab1899756768d7d5d16fb6edf3548d95f173d8e
hyukjin-2 <hyukjin.choi@does.kr>
2025-04-18 01:07:21 +0900

    [PCCE 기출문제] 9번 / 지폐 접기

---
+++ src/지폐접기.java
@@ -0,0 +1,25 @@
+class 지폐접기 {
+    public static void main(String[] args) {
+        지폐접기 sol = new 지폐접기();
+
+        int[] wallet = {50, 50};
+        int[] bill = {100, 241};
+
+        int result = sol.solution(wallet, bill);
+        System.out.println("결과: " + result);
+    }
+
+    public int solution(int[] wallet, int[] bill) {
+        int answer = 0; // 지폐를 접은 횟수
+        while (Math.min(bill[0], bill[1]) > Math.min(wallet[0], wallet[1]) || Math.max(bill[0], bill[1]) > Math.max(wallet[0], wallet[1])) {
+            if (bill[0] > bill[1]) {
+                bill[0] = bill[0] / 2;
+            } else {
+                bill[1] = bill[1] / 2;
+            }
+            answer += 1;
+        }
+        return answer;  //
+    }
+
+}
