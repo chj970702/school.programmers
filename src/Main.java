0ca163c5d0073f10ca59ca54d279f7485fa1460f
hyukjin-2 <hyukjin.choi@does.kr>
2025-04-18 18:59:06 +0900

    짝수와 홀수

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        짝수와홀수 sol = new 짝수와홀수();
+        String ans = sol.solution(2);
+        System.out.println(ans);
+    }
+}
---
+++ src/짝수와홀수.java
@@ -0,0 +1,6 @@
+public class 짝수와홀수 {
+    public String solution(int num) {
+        String answer = num % 2 == 0 ? "Even" : "Odd";
+        return answer;
+    }
+}
