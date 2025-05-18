d38c72db57830c4f787a5fe48321dbc07539e0d0
hyukjin-2 <hyukjin.choi@does.kr>
2025-05-19 00:06:48 +0900

    공원산책 리팩토링

--- src/WalkInPark.java
+++ src/WalkInPark.java
@@ -1,96 +1,65 @@
 // 공원 산책
 public class WalkInPark {
+
+    private static final int[][] DIRS = {
+            {0, 1},     // E
+            {0, -1},    // W
+            {1, 0},     // S
+            {-1, 0}     // N
+    };
+    private static final String[] DIR_NAMES = {"E", "W", "S", "N"};
+
     public int[] solution(String[] park, String[] routes) {
-        int[] answer = {};
         int[] now = {0,0};
         // 시작 위치 잡기
         for (int i=0; i<park.length; i++) {
             int idx = park[i].indexOf("S");
             if (idx != -1) {
                 now = new int[]{i, idx};
+                break;
             }
         }
+
         // 이동하기
-        for (int i=0; i<routes.length; i++) {
-            now = _after(now, routes[i], park);
+        for (String route : routes) {
+            String parts[]  = route.split(" ");
+            String dir      = parts[0];
+            int dist        = Integer.parseInt(parts[1]);
+            int dirIdx      = getDirIdx(dir);
+            int dy          = DIRS[dirIdx][0];
+            int dx          = DIRS[dirIdx][1];
+            
+            // 현재 위치
+            int ny = now[0];
+            int nx = now[1];
+            
+            // 패스여부
+            boolean valid = true;
+            
+            for (int i = 1; i <= dist; i++) {
+                ny += dy;
+                nx += dx;
+                
+                // 위치가 공원 넘어가거나, 장애물 만났는지 확인
+                if (ny < 0 || ny > park.length - 1 || nx < 0 || nx > park[0].length() -1 || park[ny].charAt(nx) == 'X') {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                now[0] = ny;
+                now[1] = nx;
+            }
         }
         return now;
     }
 
-    private int[] _after(int[] now, String route, String[] park) {
-        int[] before = new int[]{now[0], now[1]};
-        String[] parts = route.split(" ");
-        String direction = parts[0];
-        int distance = Integer.parseInt(parts[1]);
-
-        int nowWidth  = now[1];
-        int nowHeight = now[0];
-        int maxWidth = park[0].length() - 1;
-        int maxHeight = park.length - 1;
-        boolean possible = true;
-        switch (direction) {
-            case "E":
-                for (int i=1; i<=distance; i++) {
-                    // 공간 넘었는지 확인
-                    if (now[1] + 1 > maxWidth) {
-                        possible = false;
-                        break;
-                    }
-                    // 장애물 만났는지 확인
-                    else if (park[nowHeight].charAt(now[1]+1) == 'X') {
-                        possible = false;
-                        break;
-                    } else {
-                        now[1] += 1;
-                    }
-                }
-                break;
-            case "W":
-                for (int i=1; i<=distance; i++) {
-                    if (now[1] - 1 < 0) {
-                        possible = false;
-                        break;
-                    }
-                    else if (park[nowHeight].charAt(now[1]-1) == 'X') {
-                        possible = false;
-                        break;
-                    } else {
-                        now[1] -= 1;
-                    }
-                }
-                break;
-            case "N":
-                for (int i=1; i<=distance; i++) {
-                    if (now[0] - 1 < 0) {
-                        possible = false;
-                        break;
-                    }
-                    else if (park[now[0]-1].charAt(nowWidth) == 'X') {
-                        possible = false;
-                        break;
-                    } else {
-                        now[0] -= 1;
-                    }
-                }
-                break;
-            case "S":
-                for (int i=1; i<=distance; i++) {
-                    if (now[0] + 1 > maxHeight) {
-                        possible = false;
-                        break;
-                    }
-                    else if (park[now[0]+1].charAt(nowWidth) == 'X') {
-                        possible = false;
-                        break;
-                    } else {
-                        now[0] += 1;
-                    }
-                }
-                break;
+    private int getDirIdx(String dir) {
+        for (int i = 0; i < DIR_NAMES.length; i++) {
+            if (dir.equals(DIR_NAMES[i])) {
+                return i;
+            }
         }
-        if (possible) {
-            return now;
-        }
-        return before;
+        return 0;
     }
 }
