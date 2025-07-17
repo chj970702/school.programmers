489e8c9c4946099ded0b7ac986b3716544451d98
hyukjin-2 <hyukjin.choi@does.kr>
2025-07-18 01:05:45 +0900

    서버 증설 횟수 (normal)

---
+++ src/서버증설횟수.java
@@ -0,0 +1,19 @@
+public class 서버증설횟수 {
+    public int solution(int[] players, int m, int k) {
+        int answer = 0;
+        int currentServer = 0;
+        int[] expire = new int[24]; // 시간별 만료되는 서버 수
+        for (int i=0; i<24; i++) {
+            currentServer -= expire[i];
+            int needServer = players[i] / m;
+            // 필요서버가 현재 서버 수 보다 많다면
+            if (needServer > currentServer) {;
+                int add = needServer - currentServer;
+                answer += add;
+                currentServer += add;
+                if (i+k < 24) expire[i+k] += add;
+            }
+        }
+        return answer;
+    }
+}
