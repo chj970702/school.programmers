938a54b44d51324dc6620a314ca934b7a0e572f8
hyukjin-2 <hyukjin.choi@does.kr>
2025-07-18 00:27:16 +0900

    콜라 문제 (이상하게 normal)

---
+++ src/콜라문제.java
@@ -0,0 +1,11 @@
+public class 콜라문제 {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n >= a) {
+            int newCola = (n / a) * b;
+            answer += newCola;
+            n = (n % a) + newCola;  // 남은 병 + 새로 마신 콜라병 = 다음 빈병 수
+        }
+        return answer;
+    }
+}
