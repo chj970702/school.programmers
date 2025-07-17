b7f4e1e560551ec1ea33c4b85fcd3803d7fa2328
hyukjin-2 <hyukjin.choi@does.kr>
2025-07-17 15:43:03 +0900

    햄버거 만들기 (normal)

---
+++ src/햄버거만들기.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class 햄버거만들기 {
+    public int solution(int[] ingredient) {
+        /**
+         * 1: 빵
+         * 2: 야채
+         * 3: 고기
+         */
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+        for (int ing : ingredient) {
+            stack.push(ing);
+            if (stack.size() >= 4) {
+                int size = stack.size();
+                if (stack.get(size-4) == 1 &&
+                    stack.get(size-3) == 2 &&
+                    stack.get(size-2) == 3 &&
+                    stack.get(size-1) == 1
+                ) {
+                    for (int i=0; i<4; i++) {
+                        stack.pop();
+                    }
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
